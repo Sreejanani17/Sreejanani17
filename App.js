c88fba103b703dd475fd06a248fabab4accb490d
Sreejanani17 <120357306+Sreejanani17@users.noreply.github.com>
2022-12-12 12:34:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Sree()
+{
+  const x=10
+const display=<h1><center>{x%2==0? "megamoo aval" : "nee kavidhaigala"}</center></h1>
+  return(
+  display)
+}
+
+export default Sree;
